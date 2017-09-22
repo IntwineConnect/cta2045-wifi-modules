c7c7bc25886a17aa6b9ed89a2e08c5f63888943e
Ryan D. May <rmay@intwineconnect.com>
2017-09-22 17:37:55 -0400

    Fix Set RPI calculations
    Use correct bounds for input Relative Price Indicator [0.0, 9.79] and change the calculation to use floating point values

--- Aztec/BasicDR.c
+++ Aztec/BasicDR.c
@@ -665,16 +665,16 @@ RelayMsg SendPresentRelativePrice(double rpi)
     
     
     //input bounds checking
-    if(rpi > 10.0)
+    if(rpi > 9.79)
     {
-        rpi = 10.0;
+        rpi = 9.79;
     }
     else if(rpi < 0.0)
     {
         rpi = 0.0;
     }
 
-    opcode2 = (unsigned char) floor(-31 + .5*sqrtf(3844+(4*(63+8192*rpi))));    //calculate byte value per section 8.2.2 of CTA spec
+    opcode2 = (unsigned char) floor(-31.0 + 0.5*sqrtf(3844.0+(4.0*(63.0+8192.0*rpi))));    //calculate byte value per section 8.2.2 of CTA spec
     messageBuffer[5] = opcode2;
     
     RelayMsgState = RLY_WAITING_PRESENT_RELATIVE_PRICE;
@@ -698,16 +698,16 @@ RelayMsg SendNextPeriodRelativePrice(double rpi)
     
     
     //input bounds checking
-    if(rpi > 10.0)
+    if(rpi > 9.79)
     {
-        rpi = 10.0;
+        rpi = 9.79;
     }
     else if(rpi < 0.0)
     {
         rpi = 0.0;
     }
 
-    opcode2 = (unsigned char) floor(-31 + .5*sqrtf(3844+(4*(63+8192*rpi))));    //calculate byte value per section 8.2.2 of CTA spec
+    opcode2 = (unsigned char) floor(-31.0 + 0.5*sqrtf(3844.0+(4.0*(63.0+8192.0*rpi))));    //calculate byte value per section 8.2.2 of CTA spec
     messageBuffer[5] = opcode2;
     
     RelayMsgState = RLY_WAITING_NEXT_PERIOD_RELATIVE_PRICE;
